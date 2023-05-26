52b1a3e8694d7ee809d3043daa33828f81ed0358
Will Roden <will@roden.cc>
2023-05-26 18:21:42 -0500

    command and control

---
+++ cmd/goproject-tmpl/goproject-tmpl.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/alecthomas/kong"
+)
+
+var version = "unknown"
+
+const description = `` // add description here
+
+type cmdRoot struct {
+	Version kong.VersionFlag `kong:"help=${VersionHelp}"`
+}
+
+var kongVars = kong.Vars{
+	"version":     version,
+	"VersionHelp": `Output the goproject-tmpl version and exit.`,
+}
+
+func main() {
+	var cli cmdRoot
+	k := kong.Parse(&cli,
+		kongVars,
+		kong.Description(description),
+	)
+
+	fmt.Fprintln(k.Stdout, "hello, world")
+}
